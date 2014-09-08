26237f5428adb9fe813e372d25cf937f7a10ef3e
ericminio <eric.mignot@gmail.com>
2014-09-08 10:14:36 -0400

    Passes the Portfolio-Ping challenge

---
+++ Tests/Portfolio/PortfolioPingChallengeTest.cs
@@ -0,0 +1,59 @@
+using System;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Web.Http;
+using Yose;
+using System.Net.Http.Headers;
+using System.Net;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PortfolioPingChallengeTest
+    {
+        HttpResponseMessage response;
+
+        [SetUp]
+        public void TheHelloYoseChallenge() 
+        {
+            var config = new HttpConfiguration();                       
+            MvcApplication.RegisterRoutes (config);
+            var server = new HttpServer(config);
+
+            var client = new HttpClient(server);
+
+            client.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue ("application/xml"));
+            response = client.GetAsync ("http://localhost").Result;
+        }
+
+        [Test]
+        public void IsOnline()
+        {
+            Assert.That (response.StatusCode, Is.EqualTo (HttpStatusCode.OK));
+        } 
+
+        [Test]
+        public void ReturnsTextHtmlEvenIfXmlWasAsked()
+        {
+            Assert.That (response.Content.Headers.ContentType.MediaType, Is.EqualTo ("text/html"));
+        }
+
+        [Test]
+        public void DisplaysALinkTothePingChallengeImplementation ()
+        {
+            var content = ((StringContent)response.Content).ReadAsStringAsync ().Result;
+
+            var html = new HtmlDocument();
+            html.LoadHtml (content);
+            var document = html.DocumentNode;
+            var element = document.QuerySelector ("a#ping-challenge-link");
+
+            Assert.That (element, Is.Not.Null); 
+            Assert.That (element.Attributes ["href"].Value, Is.EqualTo ("ping"));
+            Assert.That (element.InnerText, Is.EqualTo ("The ping challenge"));
+        }
+    }
+}
+
